e50b0e48ae033f385b68218df5ae203cd809f9e3
alumno <alumno@utnfra.com.ar>
2019-09-24 21:58:55 -0300

    Centralita

---
+++ Centralita v1.2.5 (Ejercicio 40)/CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+  public abstract class Llamada
+  {
+    public enum TipoLlamada
+    {
+      Local,
+      Provincial,
+      Todas
+    }
+    protected float duracion;
+    protected string nroDestino;
+    protected string nroOrigen;
+
+    public abstract float CostoLlamada
+    {
+      get;
+    }
+
+    public float Duracion
+    {
+      get
+      {
+        return this.duracion;
+      }
+    }
+
+    public string NroDestino
+    {
+      get
+      {
+        return this.nroDestino;
+      }
+    }
+
+    public string NroOrigen
+    {
+      get
+      {
+        return this.nroOrigen;
+      }
+    }
+
+    public Llamada(float duracion, string nroDestino, string nroOrigen)
+    {
+      this.duracion = duracion;
+      this.nroDestino = nroDestino;
+      this.nroOrigen = nroOrigen;
+    }
+
+    public virtual string Mostrar()
+    {
+      StringBuilder str = new StringBuilder();
+      str.AppendFormat("Numero de Origen: {0}, Numero de Destino: {1}, Duración de Llamada: {2}", this.nroOrigen, this.nroDestino, this.duracion);
+      return str.ToString();
+    }
+
+    public static int OrdenarPorDuración(Llamada llamada1, Llamada llamada2)
+    {
+      return llamada1.duracion.CompareTo(llamada2.duracion);
+    }
+  }
+}
