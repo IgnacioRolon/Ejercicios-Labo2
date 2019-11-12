7a5664093c5dec720fc9543c7b891fba65bc9fb2
IgnacioRolon <ignirolon@gmail.com>
2019-11-12 17:06:26 -0300

    Ejercicio 67

--- Ejercicio 67/Ejercicio 67/Form1.cs
+++ Ejercicio 67/Ejercicio 67/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+
 
 namespace Ejercicio_67
 {
-  public partial class Form1 : Form
-  {
-    public Form1()
+    public partial class Form1 : Form
     {
-      InitializeComponent();
+        private bool flag;
+        private Temporizador temporizador;
+        private int contador = 0;
+        Random value = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            flag = false;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(flag == false)
+            {
+                temporizador = new Temporizador();
+                temporizador.eventoTiempo += OtroMetodo;
+                temporizador.Intervalo = int.Parse(textBox1.Text);
+                temporizador.Activo = true;
+            }
+            else
+            {
+                temporizador.Activo = false;
+            }
+
+            flag = !flag;
+        }
+
+        private void TestTiempo()
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            temporizador.Intervalo = int.Parse(textBox1.Text);
+        }
+
+
+        delegate void Callback();
+        public void OtroMetodo()
+        {            
+            if (this.pictureBox1.InvokeRequired || this.pictureBox2.InvokeRequired)
+            {
+                Callback d = new Callback(this.OtroMetodo);
+                this.Invoke(d);
+            }
+            else
+            {
+                Point point = new Point(pictureBox1.Location.X + value.Next(0, 10), pictureBox1.Location.Y);
+                pictureBox1.Location = point;
+
+                Thread.Sleep(50);
+
+                Point point2 = new Point(pictureBox2.Location.X + value.Next(0, 10), pictureBox2.Location.Y);
+                pictureBox2.Location = point2;
+
+                if((pictureBox1.Location.X + pictureBox1.Width) > this.Width)
+                {
+                    MessageBox.Show("Pic 1 won!");
+                    temporizador.Activo = false;
+                }else if((pictureBox2.Location.X + pictureBox2.Width) > this.Width)
+                {
+                    MessageBox.Show("Pic 2 won!");
+                    temporizador.Activo = false;
+                }
+            }
+        }
     }
-  }
 }
--- Ejercicio 67/Ejercicio 67/Temporizador.cs
+++ Ejercicio 67/Ejercicio 67/Temporizador.cs
@@ -7,35 +7,61 @@ using System.Threading.Tasks;
 
 namespace Ejercicio_67
 {
-  public sealed class Temporizador
-  {
-    private Thread hilo;
-    private int intervalo;
-
-    public bool Activo
+    public sealed class Temporizador
     {
-      get
-      {
-        return hilo.IsAlive;
-      }
-      set
-      {
-        if(value == false)
+        public delegate void encargadoTiempo();
+        public event encargadoTiempo eventoTiempo;
+
+        private Thread hilo;
+        private int intervalo;
+
+        public Temporizador()
         {
-          if(hilo.IsAlive)
-          {
-            hilo.Abort();
-          }
-        }else
-        {
-          if(!hilo.IsAlive)
-          {
-            hilo.Start();
-          }
+            hilo = new Thread(this.Corriendo);
         }
-      }
+        public bool Activo
+        {
+            get
+            {
+                return hilo.IsAlive;
+            }
+            set
+            {
+                if(value == false)
+                {
+                    if(hilo.IsAlive)
+                    {
+                        hilo.Abort();
+                    }
+                }else
+                {
+                    if(!hilo.IsAlive)
+                    {
+                        hilo.Start();
+                    }
+                }
+            }
+        }
+
+        public int Intervalo
+        {
+            get
+            {
+                return this.intervalo;
+            }
+            set
+            {
+                this.intervalo = value;
+            }
+        }
+
+        private void Corriendo()
+        {
+            while(this.Activo == true)
+            {
+                Thread.Sleep(intervalo);
+                eventoTiempo.Invoke();
+            }            
+        }
     }
-
-
-  }
 }
